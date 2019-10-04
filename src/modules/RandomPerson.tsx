4bd594dba5fe9a049af8e93d9ea55516ea987ba7
Tapudp <divyeshpar2496@gmail.com>
2019-10-04 21:50:51 +0530

    initial commit with tsconfig and dynamic app

---
+++ src/modules/RandomPerson.tsx
@@ -0,0 +1,22 @@
+import { RouteComponentProps } from "@reach/router"
+import React, { useEffect, useState } from "react"
+
+interface Props
+  extends RouteComponentProps<{
+    results: string
+  }> {}
+
+export const RandomPerson: React.FC<Props> = ({ results = 1 }) => {
+  const [person, setPerson] = useState()
+  useEffect(() => {
+    fetch(`https://randomuser.me/api?results=${results}`)
+      .then(x => x.json())
+      .then(x => setPerson(x))
+  }, [results])
+
+  return (
+    <div>
+      <pre>{JSON.stringify(person, null, 2)}</pre>
+    </div>
+  )
+}
